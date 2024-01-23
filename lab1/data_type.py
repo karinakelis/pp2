e2ec22f7df8c5c6fe202278bf9c365d76f3157a0
Karina <karinakelic@gmail.com>
2024-01-23 18:12:04 +0500

    lab1

---
+++ lab1/data_type.py
@@ -0,0 +1,11 @@
+x = 5
+print(type(x))
+
+y = "hello world"
+print(type(y))
+
+z = 20.8
+print(type(z))
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
---
+++ lab1/strings.py
@@ -0,0 +1,20 @@
+s = "hello world"
+print(len(s))
+
+txt= "hello karina"
+x = txt[0]
+print(x)
+print(txt[2:5])
+
+
+aha = "  hello teacher  "
+print(txt.strip())
+
+
+asa = "hello world"
+asa = asa.upper()  #or lower
+print(asa)
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
