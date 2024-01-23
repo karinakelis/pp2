6690e9a3e953e517064d61271f293144b2b95ca5
Karina <karinakelic@gmail.com>
2024-01-23 16:45:20 +0500

    lab1

---
+++ lab1/out_var.py
@@ -0,0 +1,24 @@
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x,y,z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 1789
+print(x + y)
+
+'''x = 5
+y = "John"
+print(x + y) will be an error'''
+
+x = 5
+y = "John"
+print(x, y) #comma is the best way
