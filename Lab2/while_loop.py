bf46b56cfcc748583b54e9adc1caf5f7850a279a
Karina <karinakelic@gmail.com>
2024-01-30 14:41:26 +0500

    lab2

---
+++ Lab2/while_loop.py
@@ -0,0 +1,21 @@
+'''i = 1
+while i < 6:
+    print (i)
+    i += 1'''
+    
+    
+i = 1
+while  i < 9:
+    if i == 3:
+        break
+    i += 1
+    print(i)
+    
+'''i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)'''
+    
+    
