58927f8d576c32fdf3f00a893a754fb8a04e10c6
Karina <karinakelic@gmail.com>
2024-01-30 23:08:10 +0500

    lab2

---
+++ Lab2/list_exercise.py
@@ -0,0 +1,51 @@
+
+#1
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+
+#2
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0]= "kiwi"
+print(fruits)
+
+
+#3
+thislist = ["apple", "banana", "cherry"]
+
+thislist.append("orange")
+
+print(thislist)
+
+
+
+
+#4
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+#5
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+
+#6
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+
+#7
+
+ruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+
+#8
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+
