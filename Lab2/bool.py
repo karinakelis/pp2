9f186b2988aea18c6fb4c19c3ab8760e1094508f
Karina <karinakelic@gmail.com>
2024-01-30 22:59:23 +0500

    lab2

--- Lab2/bool.py
+++
@@ -1,7 +0,0 @@
-a = 800
-b = 10
-if b > a:
-    print("b is greater than a")
-else:
-    print("b is not greater than a ")
-    
---
+++ Lab2/bool_exercise.py
@@ -0,0 +1,15 @@
+#1
+print(10 > 9)
+
+#2
+print(10==9)
+
+#3
+print(10 < 9)
+
+#4
+print(bool("abs"))
+
+#5
+print(bool("0"))
+    
