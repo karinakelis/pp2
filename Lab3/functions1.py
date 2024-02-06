c76374de5b75edb44d4956719e4d25dd1f4febca
Karina <karinakelic@gmail.com>
2024-02-06 13:05:22 +0500

    Lab3

--- Lab3/functions1.py
+++ Lab3/functions1.py
@@ -1,33 +1,22 @@
-
-
-
-def guess_game():
-    #random number betweer 1  and 20
-    secret_num = random.randint(1,20)
+import random
+def guessGame():
+    guessnumber=random.randint(1,20)
+    tries=0
     print("Hello! What is your name?")
-    print("Well, KBTU, I am thinking of a number between 1 and 20. Take a guess.")
-    attempt = 0
+    name=input()
+    print(f"Well, {name}, I am thinking of a number between 1 and 20")
     while True:
-        attempt += 1
-        guess = input("Enter your guess: ")
-        try:
-            guess = int(guess)
-       
-        except  ValueError:
-            print("Please enter a valid number")
+        print("Take a guess.")
+        num=int(input())
+        tries+=1
+        if(num<guessnumber):
+            print("Your guess is too low.")
+        elif(num>guessnumber):
+            print("You guess is too big")
+        else:
+            print(f"Good job, {name}! You guessed my number in {tries} guesses!")
             break
-   
-        attempt += 1
-        if guess == secret_num:
-            print("Good job, KBTU! You guessed my number in {attempt} attempts")
-            break
-        elif guess < secret_num:
-            print("Too low. Try again.")
-        elif guess > secret_num:
-            print("Too high. Try again.")
-
-guess_game()
-            
+guessGame()
               
             
         
